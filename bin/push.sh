c41cdcb940554cd8f4908cc0b0cdc40aa75054c7
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2024-08-20 00:45:48 +0530

    ci: add docker helper scripts

---
+++ bin/push.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source ./bin/variable.sh
+
+echo "Pushing image to DockerHub..."
+if [[ "$TAG" != "latest"  ]]
+then
+    echo docker push $IMAGE:$TAG
+    docker tag $IMAGE:$TAG $IMAGE:latest
+    docker push $IMAGE:$TAG
+    docker push $IMAGE:latest
+else
+    echo docker push $IMAGE:$TAG
+    docker push $IMAGE:$TAG
+fi
+
+# If the function returned err, exit
+if [ $? -ne 0 ]; then
+  echo "$IMAGE push failed!"
+  exit 1
+fi
+
+# print success
+echo "$IMAGE pushed successfully!"
